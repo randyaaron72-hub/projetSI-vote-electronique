f3e2cead71ed9870576815e89f254e2bc545741a
randyaaron72-hub <randyaaron72@gmail.com>
2025-12-11 16:19:04 +0100

    Delete railway-start.sh

--- railway-start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Simule un terminal et garde Railway heureux
-echo "1" | python3 main.py
-echo "✅ Script exécuté. Serveur en ligne."
-# Garde le conteneur en vie avec un mini serveur
-python3 -m http.server 8080
